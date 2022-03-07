6bb708202c60e1ac6ae9dfba9f7491d788ab20ad
Cihat Gündüz <commits@cihatguenduez.de>
2022-03-07 12:25:04 +0100

    Make methods public & document them

--- Sources/NonEmptySwift/Extensions/StringExt.swift
+++ Sources/NonEmptySwift/Extensions/StringExt.swift
@@ -2,13 +2,19 @@ import Foundation
 import NonEmpty
 
 extension String {
+  /// Returns a collection that guarantees to not be empty.
+  /// /// - Parameter separator: The separator to separate the String by.
+  /// - Returns: A collection that guarantees to not be empty.
   @_disfavoredOverload
-  func components<T: StringProtocol>(separatedBy separator: T) -> NonEmpty<[String]> {
+  public func components<T: StringProtocol>(separatedBy separator: T) -> NonEmpty<[String]> {
     .init(rawValue: self.components(separatedBy: separator))!
   }
 
+  /// Returns a collection that guarantees to not be empty.
+  /// - Parameter separator: The separator character set to separate the String by.
+  /// - Returns: A collection that guarantees to not be empty.
   @_disfavoredOverload
-  func components(separatedBy separator: CharacterSet) -> NonEmpty<[String]> {
+  public func components(separatedBy separator: CharacterSet) -> NonEmpty<[String]> {
     .init(rawValue: self.components(separatedBy: separator))!
   }
 }
